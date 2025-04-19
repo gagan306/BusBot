ee7731a9b04d390c00513cf32d9ea28e2e2f322d
Gagan Bhusal <bhusalgagan612@gmail.com>
2025-04-19 21:39:06 +0545

    stuck

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import smtplib  # For sending email notifications
+import smtplib 
 from datetime import datetime
 
 # Function to send an email if buses are not available
